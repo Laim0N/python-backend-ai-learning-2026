3cba5dd3ef6432e97d1151d003906e38ccdc4092
Laim0N <Laim0n1995@gmail.com>
2026-01-29 14:38:51 +1000

    Initial commit: hello.py освоил print(), input(), if/else, int()

---
+++ practice/month-01/01_hello/hello.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+print("What is your name?")
+name = input()
+print("Hello", name)
+
+print("How old are you?")
+age = int(input())
+if age < 18:
+    print("You are not an adult")
+else:
+    print("You are an adult")
