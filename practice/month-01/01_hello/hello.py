936659a4a264a3368ceac058dffc00ae30a57f9e
Laim0N <Laim0n1995@gmail.com>
2026-02-16 16:04:17 +1000

    Initial commit: Улучшил проверку введёного имени.Импортировал и вызвал функцию is_adult(), для проверки возраста.

--- practice/month-01/01_hello/hello.py
+++ practice/month-01/01_hello/hello.py
@@ -1,12 +1,16 @@
+from age_check import is_adult
 print("Hello, World!")
 
 print("What is your name?")
-name = input()
+name = input().strip().capitalize()
+if name == "" or name.isdigit():
+    print("Error: Invalid name")
+    exit()
 print("Hello", name)
 
 print("How old are you?")
 age = int(input())
-if age < 18:
-    print("You are not an adult")
-else:
+if is_adult(age):
     print("You are an adult")
+else:
+    print("You are not an adult")
